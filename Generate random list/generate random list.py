ab2a7ba6449c6f8f75aa0c0a64f8f39dec0bb8fc
NotEmirati <143645269+NotEmirati@users.noreply.github.com>
2023-09-03 12:46:48 +0400

    Update generate random list.py

--- Generate random list/generate random list.py
+++ Generate random list/generate random list.py
@@ -5,5 +5,15 @@ for i in range(0,5):
     n = random.randint(1,30)
     randomlist.append(n)
 print(randomlist)
-save = open("random_list.txt", "x")
+save = open("random_list.txt", "w")
+save.writelines(str(randomlist))
+save.close()
 
+appendlist = []
+for i in range(0,5):
+    p = random.randint(100,1000)
+    appendlist.append(p)
+print(appendlist)
+save = open('random_list.txt', 'a')
+save.writelines(str(appendlist))
+save.close()
